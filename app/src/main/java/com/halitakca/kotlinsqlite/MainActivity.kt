7a5801266ad6b4f9dc1a2f7368f03764099dc769
Halit Akca <halitakca070@gmail.com>
2023-07-01 16:19:44 +0300

    UPDATE and DELETE instructions

--- app/src/main/java/com/halitakca/kotlinsqlite/MainActivity.kt
+++ app/src/main/java/com/halitakca/kotlinsqlite/MainActivity.kt
@@ -20,8 +20,14 @@ class MainActivity : AppCompatActivity() {
             //myDataBase.execSQL("INSERT INTO musicians (name, age) VALUES('Lars',65)")
             //myDataBase.execSQL("INSERT INTO musicians (name, age) VALUES('Kirk',35)")
 
+            /*         UPDATE - DELETE
+             myDataBase.execSQL("UPDATE musicians SET age = 61 WHERE name ='Lars'")
+             myDataBase.execSQL("DELETE FROM musicians WHERE name ='Lars'")
+            */
             //val cursor = myDataBase.rawQuery("SELECT * FROM musicians WHERE id = 3 ",null)
-            val cursor = myDataBase.rawQuery("SELECT * FROM musicians WHERE name LIKE '%s' ",null)
+            //val cursor = myDataBase.rawQuery("SELECT * FROM musicians WHERE name LIKE '%s' ",null)
+
+            val cursor = myDataBase.rawQuery("SELECT * FROM musicians",null)
 
             val nameIx = cursor.getColumnIndex("name")
             val ageIx = cursor.getColumnIndex("age")
